d633f5ed01a331c42da8ede43fda3c890ec9d7d8
Ana Ordonez <ordonez4@llnl.gov>
2022-01-27 14:07:41 -0800

    add conda env

--- configuration_1/configuration1_driver.sh
+++ configuration_1/configuration1_driver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+source $CONDA_SOURCE
+conda activate $CONDA_ENV_ROOT/_CMEC_test_env
+
 python $CMEC_CODE_DIR/calculate_weighted_mean.py \
 $CMEC_MODEL_DATA/test_data_set.nc test_var \
 $CMEC_WK_DIR
